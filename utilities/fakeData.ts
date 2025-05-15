9871aa00f60f286dac4157b2e53de4d887234598
Meraj Hossain Mukto <merajhossainmukto@Merajs-MacBook-Air.local>
2025-05-16 01:28:22 +0600

    setup the base of the project

--- utilities/fakeData.ts
+++ utilities/fakeData.ts
@@ -1,11 +1,12 @@
 import { faker } from "@faker-js/faker";
+import { Page } from "@playwright/test";
 
 export class FakeUser {
   username: string;
   email: string;
   password: string;
 
-  constructor() {
+  constructor(page: Page) {
     this.username = faker.internet.username();
     this.email = faker.internet.email();
     this.password = faker.internet.password();
