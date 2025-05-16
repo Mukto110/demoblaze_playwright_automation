48131470f25b141f735413dd33d306830a3cd4bc
Meraj Hossain Mukto <merajhossainmukto@Merajs-MacBook-Air.local>
2025-05-17 00:10:06 +0600

    verify homepage reloads and resets filters on home navbar click

--- pageObjectModel/homePage.ts
+++ pageObjectModel/homePage.ts
@@ -2,8 +2,8 @@ import { Page } from "@playwright/test";
 
 export class HomePage {
   readonly homePageLogo: string;
-  readonly firstProductCard: string;
-  readonly secondProductCard: string;
+  readonly firstProductCardOfAllProduct: string;
+  readonly secondProductCardOfAllProduct: string;
   readonly navbarHome: string;
   readonly navbarContact: string;
   readonly navbarAbout: string;
@@ -15,8 +15,8 @@ export class HomePage {
 
   constructor(page: Page) {
     this.homePageLogo = `css=a[id='nava']`;
-    this.firstProductCard = `xpath=//body/div[@id='contcont']/div[@class='row']/div[@class='col-lg-9']/div[@id='tbodyid']/div[1]/div[1]`;
-    this.secondProductCard = `xpath=//body/div[@id='contcont']/div[@class='row']/div[@class='col-lg-9']/div[@id='tbodyid']/div[2]/div[1]`;
+    this.firstProductCardOfAllProduct = `xpath=//body/div[@id='contcont']/div[@class='row']/div[@class='col-lg-9']/div[@id='tbodyid']/div[1]/div[1]`;
+    this.secondProductCardOfAllProduct = `xpath=//body/div[@id='contcont']/div[@class='row']/div[@class='col-lg-9']/div[@id='tbodyid']/div[2]/div[1]`;
     this.navbarHome = `css=a[class='nav-link'][href='index.html']`;
     this.navbarContact = `css=a[data-target='#exampleModal']`;
     this.navbarAbout = `css=a[data-target='#videoModal']`;
@@ -24,6 +24,6 @@ export class HomePage {
     this.navbarLogin = `css=a[id='login2']`;
     this.navbarSignup = `css=a[id='signin2']`;
     this.categoryLaptops = `xpath=//a[@id='itemc' and text()='Laptops']`;
-    this.macbookLaptopCard = `css=body div[id='contcont'] div[class='row'] div[class='col-lg-9'] div[id='tbodyid'] div:nth-child(1) div:nth-child(1)`;
+    this.macbookLaptopCard = `css=body > div:nth-child(6) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(3)`;
   }
 }
