e5e87181d40a1a24bfe36ac95cda7e09d0a8be30
Meraj Hossain Mukto <merajhossainmukto@Merajs-MacBook-Air.local>
2025-05-29 23:46:14 +0600

    complete about us modal test

--- pageObjectModel/aboutModal.ts
+++ pageObjectModel/aboutModal.ts
@@ -8,8 +8,8 @@ export class AboutModal {
 
   constructor(page: Page) {
     this.title = `css=h5[id='videoModalLabel']`;
-    this.video = `css=div[aria-label='Modal Window'] div[role='document']`;
-    this.playButton = `css=div[aria-label='Modal Window'] div[role='document']`;
+    this.video = `css=div[class='form-group'] video[id='example-video_html5_api']`;
+    this.playButton = `css=div[class='vjs-control-bar'] button[title='Play']`;
     this.closeButton = `css=div[id='videoModal'] div[class='modal-footer'] button[type='button']`;
   }
 }
